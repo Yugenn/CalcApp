464ef76c11c0560e278b6088525e37e84f540754
Yugenn <yuruseajyan@gmail.com>
2021-11-11 09:51:29 +0900

    計算アプリ完了

--- CalcApp/SecondViewController.swift
+++ CalcApp/SecondViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 class SecondViewController: UIViewController {
 
     
+    var resultArray:[Double] = []
+    var sum:Double = 0
+    
     @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        resultArray = UserDefaults.standard.object(forKey: "item") as! [Double]
+        print(resultArray)
+        sum = resultArray.reduce(0) { (num1:Double, num2:Double) -> Double in
+            return num1 + num2
+        }
+        
+        resultLabel.text = String(format: "%.0f", sum)
     }
     
 
